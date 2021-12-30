e8ce68a1668469818659ac9106a11be20f127782
Gabriel Vasconcelos Catoni <gabriel.vasconcelos@cbdinvestimentos.com>
2021-12-30 16:41:48 -0300

    Adding cors

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,12 @@ import cors from "cors";
 
 const app = express();
 
-app.use(routes);
-app.use(express.json());
 app.use(cors({
     origin: "http://localhost:3000"
 }))
+app.use(routes);
+app.use(express.json());
+
 
 app.listen(3333, () => {
     console.log("Server is running on port 3333");
